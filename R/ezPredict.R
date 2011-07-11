ced27f85db8fe83743c0ba09bcb82d4fe4da4128
Mike Lawrence <mike.lwrnc@gmail.com>
2011-07-11 11:37:29 -0300

    ezPredict: added parametric bootstrap for both lmer and gam models, with ability to remove intercept-based variance

--- R/ezPredict.R
+++ R/ezPredict.R
@@ -3,18 +3,39 @@ function(
 	fit
 	, to_predict = NULL
 	, numeric_res = 0
-	, do_para_boot = TRUE
-	, para_boot_iterations = 1e3
+	, do_parametric_bootstrap = TRUE
+	, parametric_bootstrap_iterations = 1e3
 	, effect_variability_only = TRUE
 ){
-	data = attr(fit,'frame')
-	vars = as.character(attr(attr(data,'terms'),'variables'))
-	dv = as.character(vars[2])
+	fit_class = class(fit)[1]
+	if(fit_class=='mer'){
+		data = attr(fit,'frame')
+		vars = as.character(attr(attr(data,'terms'),'variables'))
+		dv = as.character(vars[2])
+		vars = vars[3:length(vars)]
+	}else{
+		if(fit_class=='gam'){
+			data = fit$model
+			randoms = NULL
+			for(i in fit$smooth){
+				if(class(i)[1]=='random.effect'){
+					randoms = c(randoms,i$term)
+				}
+			}
+			vars = as.character(attr(attr(data,'terms'),'variables'))
+			dv = as.character(vars[2])
+			vars = vars[3:length(vars)]
+			vars = vars[!(vars%in%randoms)]
+			BY = vars[str_detect(vars,'BY')]
+			vars = vars[!str_detect(vars,'BY')]
+		}else{
+			stop(paste('ezPredict does not know how to handle fits of class "',fit_class,'"',sep=''))
+		}
+	}
 	if(is.null(to_predict)){
 		if(length(grep('poly(',vars,fixed=TRUE))>0){
 			stop('Cannot auto-create "to_predict" when the fitted model contains poly(). Please provide a data frame to the "to_return" argument.')
 		}
-		vars = vars[3:length(vars)]
 		data_vars = vars[grep('I(',vars,fixed=T,invert=T)]
 		temp = list()
 		for(i in 1:length(data_vars)){
@@ -31,35 +52,71 @@ function(
 		}
 		to_return = data.frame(expand.grid(temp))
 		names(to_return) = data_vars
+		if(fit_class=='gam'){
+			for(i in randoms){
+				to_return$EZTEMP = data[1,names(data)==i]
+				names(to_return)[ncol(to_return)] = i
+			}
+			for(i in BY){
+				to_return$EZTEMP = ''
+				for(j in str_split(i,'BY')[[1]]){
+					to_return$EZTEMP = paste(to_return$EZTEMP,as.character(to_return[,names(to_return)==j]))
+				}
+				to_return$EZTEMP = ordered(to_return$EZTEMP)
+				names(to_return)[ncol(to_return)] = i
+			}
+		}
 	}else{
 		to_return = to_predict
 		data_vars = names(to_predict)
 	}
-	requested_terms = terms(eval(parse(text=paste(
-		dv
-		, '~'
-		, paste(
-			attr(attr(data,'terms'),'term.labels')
-			, collapse = '+'
-		)
-	))))
 	to_return$ezDV = 0
 	names(to_return)[ncol(to_return)] = dv
-	mm = model.matrix(requested_terms,to_return)
-	f = fixef(fit)
+	if(fit_class=='mer'){
+		requested_terms = terms(eval(parse(text=paste(
+			dv
+			, '~'
+			, paste(
+				attr(attr(data,'terms'),'term.labels')
+				, collapse = '+'
+			)
+		))))
+		mm = model.matrix(requested_terms,to_return)
+		f = fixef(fit)
+		v = vcov(fit)
+		if(effect_variability_only){
+			v[1,] = 0 #eliminate intercept variance
+			v[,1] = 0 #eliminate intercept variance
+		}		
+	}else{
+		mm <- predict(fit,to_return,type="lpmatrix") # get a coefficient matrix
+		for(i in randoms){
+			mm[,grep(paste('s(',i,')',sep=''),dimnames(mm)[[2]],fixed=T)] = 0 #zero the subject entry	
+		}
+		f = coef(fit)
+		for(i in randoms){
+			f[grep(paste('s(',i,')',sep=''),names(f),fixed=T)] = 0 #zero the subject entry	
+		}
+		v <- vcov(fit) # posterior cov of coefs 
+		if(effect_variability_only){
+			v[1,] = 0 #eliminate intercept variance
+			v[,1] = 0 #eliminate intercept variance
+		}
+		for(i in randoms){
+			v[grep(paste('s(',i,')',sep=''),dimnames(v)[[1]],fixed=T),] = 0 #zero the subject entry	
+			v[,grep(paste('s(',i,')',sep=''),dimnames(v)[[2]],fixed=T)] = 0 #zero the subject entry	
+		}
+	}
 	value = mm %*% f
 	to_return$value = as.numeric(value[,1])
-	vf = vcov(fit)
-	tc = Matrix::tcrossprod(vf,mm)
+	tc = Matrix::tcrossprod(v,mm)
 	to_return$var = Matrix::diag(mm %*% tc)
-	to_return = to_return[,names(to_return)!=dv]
 	to_return = to_return[,names(to_return) %in% c(data_vars,'value','var')]
-	if(do_para_boot){
-		from_sim = arm::sim(fit,n=para_boot_iterations)
-		f = t(fixef(from_sim))
-		mat = matrix(NA,nrow=nrow(to_return),ncol=para_boot_iterations)
-		for(i in 1:para_boot_iterations){
-			mat[,i] = as.numeric((mm%*%f[,i])[,1])
+	if(do_parametric_bootstrap){
+		samples = mvrnorm(parametric_bootstrap_iterations,f,v)
+		mat = matrix(NA,nrow=nrow(to_return),ncol=parametric_bootstrap_iterations)
+		for(i in 1:parametric_bootstrap_iterations){
+			mat[,i] <- mm%*%samples[i,]
 		}
 		if(effect_variability_only){
 			mat = t(t(mat)-colMeans(mat))+mean(mat) #remove variability attributable to iteration grand mean
@@ -69,13 +126,13 @@ function(
 		boots = cbind(boots,as.data.frame(mat))
 		boots = melt(
 			data = boots
-			, id.vars = names(boots)[1:(ncol(boots)-para_boot_iterations)]
+			, id.vars = names(boots)[1:(ncol(boots)-parametric_bootstrap_iterations)]
 			, variable_name = 'iteration'
 		)
 		to_return = list(
 			cells = to_return
 			, boots = boots
-		)
+		)		
 	}
 	return(to_return)
 }
