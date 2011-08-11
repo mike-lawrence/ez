459786ec8786c4364d7a1bca09849a833b06c11f
Mike Lawrence <mike.lwrnc@gmail.com>
2011-08-10 23:34:56 -0300

    Added ezDev for easy downloading/source-ing of the latest development code for ez.

---
+++ R/ezDev.R
@@ -0,0 +1,20 @@
+ezDev <- function(){
+	temp = getURL('https://github.com/mike-lawrence/ez/tree/master/R')
+	temp = strsplit(temp,'\n')[[1]]
+	temp = temp[temp!='']
+	temp = temp[str_detect(temp,'            <td class="content"> <a href="/mike-lawrence/ez/blob/')]
+	files = str_extract(temp,'/R/.*R\\" class')
+	files = str_replace(files,'/R/','')
+	files = str_replace(files,'\\" class','')
+	for(this_file in files){
+		this_file = paste(
+			'https://raw.github.com/mike-lawrence/ez/master/R/'
+			, this_file
+			, sep = ''
+		)
+		eval(
+			expr = parse(text=getURL(this_file))
+			, envir = .GlobalEnv
+		)
+	}
+}
